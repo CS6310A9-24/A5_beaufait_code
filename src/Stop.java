de683db15d3e741beb21818e6e3059a22ea09d5e
unbreakableIce <terrence.bunkley@gmail.com>
2018-11-19 00:06:09 -0600

    add members and function for distributions

--- src/Stop.java
+++ src/Stop.java
@@ -1,6 +1,15 @@
 public class Stop {
     // Declare Stop attributes
     private int id;
+    private int ridersArriveLow;
+    private int ridersArriveHigh;
+    private int ridersOffLow;
+    private int ridersOffHigh;
+    private int ridersOnLow;
+    private int ridersOnHigh;
+    private int ridersDepartLow;
+    private int ridersDepartHigh;
+
     private String name;
     private int numPassengersWaiting;
     private int numNewPassengerArrivals;
@@ -40,5 +49,105 @@ public class Stop {
         stopLocation[0] = location.getLatitude();
         stopLocation[1] = location.getLongitude();
         return stopLocation;
+    }
+
+    public void setStopDistributions(int ridersArriveHigh, int ridersArriveLow, int ridersOffHigh, int ridersOffLow,
+                                     int ridersOnHigh, int ridersOnLow, int ridersDepartHigh, int ridersDepartLow) {
+        this.ridersArriveHigh = ridersArriveHigh;
+        this.ridersArriveLow = ridersArriveLow;
+        this.ridersOffHigh = ridersOffHigh;
+        this.ridersOffLow = ridersOffLow;
+        this.ridersOnHigh = ridersOnHigh;
+        this.ridersOnLow = ridersOnLow;
+        this.ridersDepartHigh = ridersDepartHigh;
+        this.ridersDepartLow = ridersDepartLow;
+    }
+
+    public int getRidersArriveLow() {
+        return ridersArriveLow;
+    }
+
+    public void setRidersArriveLow(int ridersArriveLow) {
+        this.ridersArriveLow = ridersArriveLow;
+    }
+
+    public int getRidersArriveHigh() {
+        return ridersArriveHigh;
+    }
+
+    public void setRidersArriveHigh(int ridersArriveHigh) {
+        this.ridersArriveHigh = ridersArriveHigh;
+    }
+
+    public int getRidersOffLow() {
+        return ridersOffLow;
+    }
+
+    public void setRidersOffLow(int ridersOffLow) {
+        this.ridersOffLow = ridersOffLow;
+    }
+
+    public int getRidersOffHigh() {
+        return ridersOffHigh;
+    }
+
+    public void setRidersOffHigh(int ridersOffHigh) {
+        this.ridersOffHigh = ridersOffHigh;
+    }
+
+    public int getRidersOnLow() {
+        return ridersOnLow;
+    }
+
+    public void setRidersOnLow(int ridersOnLow) {
+        this.ridersOnLow = ridersOnLow;
+    }
+
+    public int getRidersOnHigh() {
+        return ridersOnHigh;
+    }
+
+    public void setRidersOnHigh(int ridersOnHigh) {
+        this.ridersOnHigh = ridersOnHigh;
+    }
+
+    public int getRidersDepartLow() {
+        return ridersDepartLow;
+    }
+
+    public void setRidersDepartLow(int ridersDepartLow) {
+        this.ridersDepartLow = ridersDepartLow;
+    }
+
+    public int getRidersDepartHigh() {
+        return ridersDepartHigh;
+    }
+
+    public void setRidersDepartHigh(int ridersDepartHigh) {
+        this.ridersDepartHigh = ridersDepartHigh;
+    }
+
+    public void setNumPassengersWaiting(int numPassengersWaiting) {
+        this.numPassengersWaiting = numPassengersWaiting;
+    }
+
+    public int getNumNewPassengerArrivals() {
+        return numNewPassengerArrivals;
+    }
+
+    public void setNumNewPassengerArrivals(int numNewPassengerArrivals) {
+        this.numNewPassengerArrivals = numNewPassengerArrivals;
+    }
+
+    public int getNumPassengerDepartures() {
+        return numPassengerDepartures;
+    }
+
+    public void setNumPassengerDepartures(int numPassengerDepartures) {
+        this.numPassengerDepartures = numPassengerDepartures;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 }
