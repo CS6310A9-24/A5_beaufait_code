da509a87532f377aced76a5317fa1dfe8302f5d3
unbreakableIce <terrence.bunkley@gmail.com>
2018-11-21 15:05:42 -0600

    Removed unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-import java.util.Scanner;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.lang.*;
-
 public class Main {
 
     public static void main(String[] args) {
