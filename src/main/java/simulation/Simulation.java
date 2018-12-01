4a140193e8ac42599f402bf15a71b8dc0694b6b6
comtety <tyler.comte@gmail.com>
2018-12-01 14:22:18 -0600

    Added UI capabilities for route changes

--- src/main/java/simulation/Simulation.java
+++ src/main/java/simulation/Simulation.java
@@ -34,7 +34,8 @@ public class Simulation {
     public void setup(String[] args) {
 
         final String DELIMITER = ",";
-        String scenarioFile = args[0];
+        //String scenarioFile = args[0];
+        String scenarioFile = "resources/test_scenario.txt";
         // Step 1: Read the data from the provided scenario configuration file.
         try {
             Scanner takeCommand = new Scanner(new File(scenarioFile));
@@ -88,8 +89,8 @@ public class Simulation {
 
         // Step 2: Read the data from the provided passenger probabilities file
         final String PASSENGER_PROBABILITY_DELIMITER = ",";
-        String probabilityFile = args[1];
-
+        //String probabilityFile = args[1];
+        String probabilityFile = "results/test_evening_distibution.csv";
         try {
             Scanner takeCommand = new Scanner(new File(probabilityFile));
             String[] tokens;
